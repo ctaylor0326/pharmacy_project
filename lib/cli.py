baaf370db4a74e14f0adc0da5385fab3a776ba5b
CahenTaylor <cahentaylor@gmail.com>
2023-05-17 13:31:29 -0600

    tables created

--- lib/cli.py
+++ lib/cli.py
@@ -1,2 +1,37 @@
+from helpers import *
+from db.models import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///pharmacy.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
 if __name__ == '__main__':
-    print('Welcome to Walgreenz!')
+    print(greeting_image)
+    print()
+    print('W e l c o m e   t o   W a l g r e e n z !')
+    print()
+
+    # retrieve patient object from the DB
+    patient = None
+    while not patient:
+        patient_id = input('Please enter your patient ID: ')
+        patient = session.query(Patient).filter(Patient.id == patient_id).one_or_none()
+        
+
+    # display patient info
+    print()
+    print()
+    print()
+    print()
+    print(f'Welcome back, {patient.first_name} {patient.last_name}!')
+    print()
+    print(f"Your prescriptions on file are:")
+    print('-' * 50)
+
+    # display prescription table
+    create_prescription_table(patient)
+
+    # display fill prescription menu
+    fill_prescription_menu()
